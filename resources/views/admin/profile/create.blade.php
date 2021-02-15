554fe95c5de5122de449725b0f45099c3bec488c
tomoya.nagata2 <tomoyarenofa@gmail.com>
2021-02-15 03:29:32 +0000

    kdai20

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,13 +1,14 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" contenr="IE-edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    
-    <title>My Profile</title>
-  </head>
-  <body>
-    <h1>プロフィール作成画面</h1>
-  </body>
-</html>
+@extends('layouts.admin')
+
+@section('title', 'Ｍｙプロフィール')
+
+
+@section('content')
+ <div class="container">
+   <div class="row">
+    　<div class="col-md-8 mx-auto">
+      　<h2>Myプロフィール</h2>
+    　</div>
+  　</div> 
+ </div>
+@endsection 
